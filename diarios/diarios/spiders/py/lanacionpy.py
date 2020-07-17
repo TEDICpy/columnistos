09c550fa27518642c7a2875f64e4ee386f12ab2b
Lu <lupa@inventati.org>
2020-07-17 14:51:32 -0300

    corregida lanacionpy

--- diarios/diarios/spiders/py/lanacionpy.py
+++ diarios/diarios/spiders/py/lanacionpy.py
@@ -21,10 +21,11 @@ class LanacionpySpider(scrapy.Spider):
         @returns requests 0 0
         @scrapes author title url
         """
-        # Esta búsqueda se queda con todo lo que está debajo del arbol cuya id es west (panel principal)
+        # Esta búsqueda se queda con todo lo que está debajo del arbol cuya class es text-cont
         # de esto busca todos los articulos, pero filtra el primer div y el enlace
-        selectors = response.xpath('//*[@id="west"]//article/div/a[2]')
+        selectors = response.xpath('//*[@class="text-cont"]')
         # Recorro los resultados y extraigo la URL
+        # OBS: Devuelve una URL relativa
         for selector in selectors:
             link = response.urljoin(selector.xpath('.//@href').extract_first())
             if link is not None:
@@ -32,10 +33,10 @@ class LanacionpySpider(scrapy.Spider):
 
     def parse_article(self, response):
         import re
-        selector = response.xpath('//*[@id="article-content"]')
+        selector = response.xpath('//*[@class="article-content"]')
         loader = ItemLoader(DiariosItem(), selector=selector)
         #Extraigo autor y convierto en mayus y borro espacios
-        autor = response.xpath('.//span[@class="author-name"]//text()').extract_first().title().strip()
+        autor = response.xpath('//*[@class="name"]//text()').extract_first().title().strip()
         # Saco símbolos raros
         autor = re.sub('[^a-zA-ZñÑáéíóúÁÉÍÓÚ ]', '', autor)
         # Trae "Por" al principio así que lo saco
@@ -44,7 +45,7 @@ class LanacionpySpider(scrapy.Spider):
         # Guardo autor
         loader.add_value('author', autor)
         # Guardo título
-        loader.add_value('title', response.xpath('//*[@class="title"]/h1/text()').extract_first().strip())
+        loader.add_value('title', response.xpath('//*[@class="headline"]//text()').extract_first().strip())
         # Guardo URL
         loader.add_value('url', response.request.url)
         return loader.load_item()
